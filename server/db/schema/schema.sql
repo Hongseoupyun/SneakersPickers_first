6aff70832f379ce1e039b7bcdfe21e36688312c1
Nhan <nktran4@gmail.com>
2022-06-09 17:45:48 -0400

    components add

--- server/db/schema/schema.sql
+++ server/db/schema/schema.sql
@@ -8,14 +8,14 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-  picture VARCHAR(255) 
+  password VARCHAR(255) NOT NULL,
+  picture VARCHAR(255)
 );
 
 CREATE TABLE notes (
   id SERIAL PRIMARY KEY NOT NULL,
   users_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  task VARCHAR(255),
+  task VARCHAR(255)
 );
 
 CREATE TABLE projects (
@@ -28,11 +28,11 @@ CREATE TABLE projects (
 CREATE TABLE contacts (
   id SERIAL PRIMARY KEY NOT NULL,
   users_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  contact_link VARCHAR(255),
+  contact_link VARCHAR(255)
 );
 
 CREATE TABLE todos (
   id SERIAL PRIMARY KEY NOT NULL,
   users_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  task VARCHAR(255),
+  task VARCHAR(255)
 );
