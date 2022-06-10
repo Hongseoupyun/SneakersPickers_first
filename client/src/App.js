32a69819065851ff02b377293e5e61f2c223e472
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-10 18:58:30 -0400

    Add react-router-dom and routes for MyAccount

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import {Route, Link} from 'react-router-dom';
+import MyAccount from './MyAccount';
 function App() {
   return (
     <div className="App">
-      
+      <Router to ="/MyAccount" component={MyAccount}/>
     </div>
   );
 }
