1ad58314b84177227454e2767d66c0a6aa9f5970
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-10 23:23:45 -0400

    Add components and routes

--- client/src/components/MyAccount.js
+++ client/src/components/MyAccount.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import './MyAccount/SideBar'
+import SideBar from './MyAccount/SideBar'
+import Profile from './Profile'
+import './MyAccount/myAccountPage.css'
 
-export default function MyAccount (){
-  return(
-    <div>
-      MyAccountlksjlf
-    </div>
+export default function MyAccount() {
+  return (
+
+    <div className='myAccountPage'>
+      <section className='sideBar'>
+        <nav>
+          <SideBar/>
+        </nav>
+      </section>
+
+      <section className='profile'>
+        <Profile/>
+      </section>
+
+      
+      </div>
+
   )
 }
 
