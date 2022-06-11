b6d05bfda7e1e83529f951e41911e038dc29f2fb
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-10 23:45:08 -0400

    Pull Navbar from nhanmain

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Logform from "./Logform";
-import Profile from "./Profile";
-import Offers from "./MyAccount/Offers"
-import MyListings from "./MyAccount/MyListings"
-import AddAListing from "./MyAccount/AddAListing"
-
-
+import Profile from "./MyAccount/Profile";
+import Offers from "./MyAccount/Offers";
+import MyListings from "./MyAccount/MyListings";
+import AddAListing from "./MyAccount/AddAListing";
 
 const Main = () => {
   return (
     <Routes>
-      <Route path='/' element={<Logform/>}></Route>
-      <Route path='/profile' element={<Profile/>}></Route>
-      <Route path='/offers' element={<Offers/>}></Route>
-      <Route path='/mylistings' element={<MyListings/>}></Route>
-      <Route path='/addalisting' element={<AddAListing/>}></Route>
+      <Route path="/" element={<Logform />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+      <Route path="/offers" element={<Offers />}></Route>
+      <Route path="/mylistings" element={<MyListings />}></Route>
+      <Route path="/addalisting" element={<AddAListing />}></Route>
     </Routes>
   );
-}
+};
 
 export default Main;
-
