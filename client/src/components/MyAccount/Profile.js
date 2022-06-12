e6a199d0136754ed3494f1f267841d985fb91411
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-12 00:53:35 -0400

    Add css to Profile

--- client/src/components/MyAccount/Profile.js
+++ client/src/components/MyAccount/Profile.js
@@ -1,13 +1,44 @@
 import React from "react";
+import "./Profile.scss";
 
 export default function Profile() {
   return (
-    <section className="profile">
-      <h1>Profile</h1>
-      <h2>Name</h2>
-      <h2>Email</h2>
-      <h2></h2>
+    <body className="profile-body">
+    <section class="login-form">
+      <h1 className="profile-heading">Profile</h1>
+      <form action="/login" method = "POST">
+        <div class="int-area">
+          <input type="text" name ="email" id="id" autocomplete="off" required/>
+          <label>Name </label>
+        </div>
+        <div class="int-area">
+          <input type="password" name ="pw" id="pw " autocomplete="off" required/>
+          <label>Email</label>
+        </div> 
+        <div class="btn-area">
+          <button type="submit" >Edit</button>
+        </div>
+        <div class="int-area">
+          <input type="password" name ="email" id="id" autocomplete="off" required/>
+          <label>Current Password</label>
+        </div>
+        <div class="int-area">
+          <input type="password" name ="pw" id="pw " autocomplete="off" required/>
+          <label>New Password </label>
+        </div> 
+        <div class="int-area">
+          <input type="password" name ="pw" id="pw " autocomplete="off" required/>
+          <label>Confirm New Password</label>
+        </div> 
+        <div class="btn-area">
+          <button type="submit" >Reset Password</button>
+        </div>
+      </form>
+      
+  
     </section>
+    
+  </body>
       
   );
 }
