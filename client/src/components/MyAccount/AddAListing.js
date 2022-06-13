e44bc1850199e77b1824203b6e10b74e3683c7d3
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-12 20:36:11 -0400

    Edit css for Profile and Add html for AddAListing

--- client/src/components/MyAccount/AddAListing.js
+++ client/src/components/MyAccount/AddAListing.js
@@ -3,52 +3,78 @@ import "./AddAListing.scss";
 
 function AddAListing() {
   return (
-    <body className="Add-body">
+    <body className="add-body">
       <div className="master-container">
-        <div>
-          <img src="client/src/components/images/sneakers2.jpeg" />
+        <div className="img-div">
+          <img src="/Users/hongseoupyun/lighthouse/SneakersPickers/client/src/components/images/sneakers2.jpeg"/>
         </div>
         <form className="form">
           <h3>Uploade your shoes to trade</h3>
-          <br />
-          <label for="shoes-name">Shoes Name</label>
-          <br />
-          <input type="text" name="shoes-name" />
-          <div className="size-brand">
-            <select name="Size">
-              <option>Size 4</option>
-              <option>Size 5</option>
-              <option>Size 6</option>
-              <option>Size 7</option>
-              <option>Size 8</option>
-              <option>Size 9</option>
-              <option>Size 10</option>
-              <option>Size 11</option>
-              <option>Size 12</option>
-            </select>
-            <select name="Brand">
-              <option>Air Jordan</option>
-              <option>Nike Dunks</option>
-              <option>Nike Air Force 1</option>
-              <option>Adidas Yeezy</option>
-              <option>New balances</option>
-              <option>Vans/Converse</option>
-              <option>Others</option>
-            </select>
+          <div className="int-area">
+            <input
+              type="text"
+              name="shoes-name"
+              autocomplete="off"
+              required
+            />
+            <label>Name</label>
           </div>
-          <label for="description">Description</label>
-          <br />
-          <input type="text" name="description" />
-          <br />
-          <label for="img-url">Image Url</label>
-          <br />
-          <input type="text" name="img-url" />
-          <br />
-          <label for="preferences">Preferences</label>
-          <br />
-          <input type="text" name="preferences" />
-          <br />
-          <button>Publish</button>
+          <div className="size-brand">
+            <div>
+              <select name="size" className="size">
+                <option>Size 4</option>
+                <option>Size 5</option>
+                <option>Size 6</option>
+                <option>Size 7</option>
+                <option>Size 8</option>
+                <option>Size 9</option>
+                <option>Size 10</option>
+                <option>Size 11</option>
+                <option>Size 12</option>
+              </select>
+            </div>
+            <div>
+              <select name="brand" className="brand">
+                <option>Air Jordan</option>
+                <option>Nike Dunks</option>
+                <option>Nike Air Force 1</option>
+                <option>Adidas Yeezy</option>
+                <option>New balances</option>
+                <option>Vans/Converse</option>
+                <option>Others</option>
+              </select>
+            </div>
+          </div>
+          <div className="int-area">
+            <input
+              type="text"
+              name="description"
+              autocomplete="off"
+              required
+            />
+            <label>Description</label>
+          </div>
+          <div className="int-area">
+            <input
+              type="text"
+              name="img-url"
+              autocomplete="off"
+              required
+            />
+            <label>Image Url</label>
+          </div>
+          <div className="int-area">
+            <input
+              type="text"
+              name="pref"
+              autocomplete="off"
+              required
+            />
+            <label>Preference(eg.Nike Dunks Varisty red etc.)</label>
+          </div>
+          <div className="btn-area">
+            <button type="submit">Publish</button>
+          </div>
         </form>
       </div>
     </body>
--- client/src/components/MyAccount/Profile.js
+++ client/src/components/MyAccount/Profile.js
@@ -6,32 +6,29 @@ export default function Profile() {
     <body className="profile-body">
     <section class="login-form">
       <h1 className="profile-heading">Profile</h1>
-      <form action="/login" method = "POST">
-        <div class="int-area">
-          <input type="text" name ="email" id="id" autocomplete="off" required/>
-          <label>Name </label>
+      <form action="/profile" method = "POST">
+        <div className="int-area">
+          <input type="text" name ="user-name" autocomplete="off" required/>
+          <label>Name</label>
         </div>
-        <div class="int-area">
-          <input type="password" name ="pw" id="pw " autocomplete="off" required/>
+        <div className="int-area">
+          <input type="password" name ="email" autocomplete="off" required/>
           <label>Email</label>
         </div> 
-        <div class="btn-area">
-          <button type="submit" >Edit</button>
-        </div>
-        <div class="int-area">
-          <input type="password" name ="email" id="id" autocomplete="off" required/>
+        <div className="int-area">
+          <input type="password" name ="password" autocomplete="off" required/>
           <label>Current Password</label>
         </div>
-        <div class="int-area">
-          <input type="password" name ="pw" id="pw " autocomplete="off" required/>
+        <div className="int-area">
+          <input type="password" name ="new-password" autocomplete="off" required/>
           <label>New Password </label>
         </div> 
-        <div class="int-area">
-          <input type="password" name ="pw" id="pw " autocomplete="off" required/>
+        <div className="int-area">
+          <input type="password" name ="new-password-confirm" autocomplete="off" required/>
           <label>Confirm New Password</label>
         </div> 
-        <div class="btn-area">
-          <button type="submit" >Reset Password</button>
+        <div className="btn-area">
+          <button type="submit" >Save Changes</button>
         </div>
       </form>
       
