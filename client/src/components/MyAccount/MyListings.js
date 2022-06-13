07f1816ddb0839a998bf50b7d6487d365005b33b
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-13 01:09:35 -0400

    Add html and css for MyListings

--- client/src/components/MyAccount/MyListings.js
+++ client/src/components/MyAccount/MyListings.js
@@ -1,8 +1,17 @@
 import React from "react";
-
+import myListingImg from "../images/mylisting-img-sample.webp"
+import "./MyListings.scss"
 
 export default function MyListings() {
   return (
-      <div>mylistings</div>
+      <div className="mylisting-container">
+        <div className="mylisting-img-card">
+          <img className="mylisting-img" src={myListingImg}/>
+        </div>
+        <div className="mylisting-card-contents">
+
+        </div>
+
+      </div>
   );
 }
