d5bbea3309e7ad606a3f47504f1182fa0e9c6aa3
Nhan <nktran4@gmail.com>
2022-06-13 01:55:33 -0400

    browsing page layout update

--- client/src/components/browse/browse.js
+++ client/src/components/browse/browse.js
@@ -1,128 +1,55 @@
 import React, { Fragment } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './browse.scss'
+import runlogo from '../images/runlogo.gif'
 
 function Browse() {
   return (
-    <Fragment>
-      <div class="vertical-menu">
-        <span className="filter">Filter list</span><br/>
-
-        <span className="filtername">Brand</span>
-
-        <form className="filter">
-          <form className="brandcheck" id="filtered">
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="brand" id="flexRadioDefault1" value="airjordan"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-                Air Jordan
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="brand" id="flexRadioDefault2" value="nikedunks" />
-              <label class="form-check-label" for="flexRadioDefault2">
-                Nike Dunks
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="brand" id="flexRadioDefault1" value="nikeairforce"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              Nike Air Force
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="brand" id="flexRadioDefault1" value="adidasyeezy"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              Adidas Yeezy
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="brand" id="flexRadioDefault1" value="newbalances"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              New Balances
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="brand" id="flexRadioDefault1" value="vans"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              Vans
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="brand" id="flexRadioDefault1" value="others"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              Others
-              </label>
-            </div>
-          </form>
-
-          <span className="filtername">Size</span>
-
-          <form className="sizecheck">
-          <div class="form-check">
-              <input class="form-check-input" type="radio" name="size" id="flexRadioDefault1" value="4"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              4
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="size" id="flexRadioDefault1" value="5"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              5
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="size" id="flexRadioDefault1" value="6"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              6
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="size" id="flexRadioDefault1" value="7"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              7
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="size" id="flexRadioDefault1" value="8"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              8
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="size" id="flexRadioDefault1" value="9"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              9
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="size" id="flexRadioDefault1" value="10"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              10
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="size" id="flexRadioDefault1" value="11"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              11
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="size" id="flexRadioDefault1" value="12"/>
-              <label class="form-check-label" for="flexRadioDefault1">
-              12
-              </label>
-            </div>
-          </form>
-          <button type="submit" form="filtered" class="btn btn-secondary mt-3">Filter</button>
-          <button type="button" href="/browse/" class="btn btn-secondary mt-3">Reset</button>
-        </form>
+    <header className="layout">
+      <header class="vertical-menu" sticky="fixed">
         
-      </div>
+          <span className="filter">Filter list</span><br/>
+
+          <label for="brand">Pick a brand:</label>
+          <select name="brand" id="brand">
+            <optgroup label="Brand">
+              <option value="air jordan">Air Jordan</option>
+              <option value="nike dunks">Nike Dunks</option>
+              <option value="nike air force">Nike Air Force</option>
+              <option value="adidas yeezy">Adidas Yeezy</option>
+              <option value="new balances">New Balances</option>
+              <option value="vans">Vans</option>
+              <option value="others">Others</option>
+            </optgroup> 
+          </select><br/>
+
+          <label for="size" placeholder="Size">Pick a size:</label>
+          <select name="size" id="size">
+            <optgroup label="Size">
+              <option value="4">Size 4</option>
+              <option value="5">Size 5</option>
+              <option value="6">Size 6</option>
+              <option value="7">Size 7</option>
+              <option value="8">Size 8</option>
+              <option value="9">Size 9</option>
+              <option value="10">Size 10</option>
+              <option value="11">Size 11</option>
+              <option value="12">Size 12</option>
+            </optgroup>
+          </select><br/>
+
+        <div className="buttons">
+          <button type="submit" class="btn btn-secondary mt-3">Filter</button>
+          <button type="button" href="/browse/" class="btn btn-secondary mt-3">Reset</button>
+        </div>
+        <img src={runlogo} alt="run" className="runlogo"></img>
+      </header>
 
 
-      
-    </Fragment>
+      <header className="tradecard">
+        Hello
+      </header>
+    </header>
   );
 }
 
