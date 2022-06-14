4720e84da0924ebf5f275584cc3e4d3686d9f50a
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-14 19:44:42 -0400

    Add Profile Edit Feature

--- client/src/components/MyAccount/Profile.js
+++ client/src/components/MyAccount/Profile.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "./Profile.scss";
 import axios from "axios";
-import ProfileItem from "./ProfileItem";
-
 
 export default function Profile() {
-  //set state of the data
-  const [usersProfile, setUsersProfile] = useState({});
+
+  const [currentPassword,setCurrentPassword] = useState("")
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [newPassword, setnewPassword] = useState("")
+  const [confirmedPassword, setconfirmedPassword] = useState("")
 
   //fetch data from api using axios
   const getUsersProfile = function () {
     return axios.get("/api/profile")
       .then((result) => {
-        console.log(result.data);
-        setUsersProfile(result.data);
+        setName(result.data.name)
+        setEmail(result.data.email)
       });
   };
   //load usersProfile when rendering component
@@ -21,12 +23,72 @@ export default function Profile() {
     getUsersProfile();
   }, []);
 
+ //sending updated dato to api
+  const saveChanges = function (e) {
+    e.preventDefault();
+    axios.post("api/profile", { name, email, currentPassword, newPassword, confirmedPassword })
+      .then((res) => {
+        console.log(res.data)
+        setnewPassword("")
+        setCurrentPassword("")
+        setconfirmedPassword("")
+      })
+      .catch((err)=>{
+        console.log("error!=>",err)
+      })
+  }
+ 
   return (
     <div className="profile-body">
-      <ProfileItem
-        name={usersProfile.name}
-        email={usersProfile.email}
-      />
+      <section className="login-form">
+      <h1 className="profile-heading">Profile</h1>
+      <form action="/api/profile" method="POST" onSubmit={saveChanges}>
+        <div className="int-area">
+          <input type="text" name="user-name" value={name} onChange={(e) => { setName(e.target.value) }} autoComplete="off" required />
+          <label>Name</label>
+        </div>
+        <div className="int-area">
+          <input type="email" name="email" value={email} onChange={(e) => { setEmail(e.target.value) }} autoComplete="off" required />
+          <label>Email</label>
+        </div>
+        <div className="int-area">
+          <input
+            type="password"
+            name="password"
+            value={currentPassword}
+            onChange={(e) => { setCurrentPassword(e.target.value) }}
+            autoComplete="off"
+            required
+          />
+          <label>Current Password</label>
+        </div>
+        <div className="int-area">
+          <input
+            type="password"
+            name="new-password"
+            value={newPassword}
+            onChange={(e) => { setnewPassword(e.target.value) }}
+            autoComplete="off"
+            required
+          />
+          <label>New Password </label>
+        </div>
+        <div className="int-area">
+          <input
+            type="password"
+            name="new-password-confirm"
+            value={confirmedPassword}
+            onChange={(e) => { setconfirmedPassword(e.target.value) }}
+            autoComplete="off"
+            required
+          />
+          <label>Confirm New Password</label>
+        </div>
+        <div className="btn-area">
+          <button type="submit" onClick={(e) => { saveChanges(e) }}>Save Changes</button>
+        </div>
+      </form>
+    </section>
     </div>
   );
 }
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 
 let queryUser = `SELECT * FROM users WHERE id = $1`;
-let updateUserProfile = ``
+let updateUserProfile = `UPDATE users
+SET name = $1, email= $2, password= $3
+WHERE id = $4;`
 
 module.exports = (db) => {
+
   router.get("/profile", (req, res) => {
-    console.log(req.user)
-    db.query(queryUser, [3])
+    console.log("profile user:", req.user.id)
+    db.query(queryUser, [req.user.id])
       .then((result) => {
         res.json(result.rows[0]);
       })
@@ -14,11 +17,23 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
+
   router.post("/profile",(req,res)=>{
-    const {newName, newEmail, newPassword, confirmedPassword}= req.body
-    // if(newPassword === confirmedPassword) {
-      db.query(updateUserProfile,[newName, newEmail, newPassword])
-    // }
+    const {name, email, newPassword, confirmedPassword, currentPassword}= req.body
+    db.query(`SELECT password FROM users where id = $1`,[req.user.id])
+    .then((result)=>{
+      const passwordInDb=result.rows[0].password
+      if(newPassword === confirmedPassword && currentPassword === passwordInDb ) {
+        console.log(req.body)
+        db.query(updateUserProfile,[name, email, newPassword, req.user.id])
+        .then(()=>{
+          res.json({success:true, message:"Profile Updated"})
+        })
+        .catch((err)=>{
+          console.log("Error! ->",err)
+        })
+      }
+    })
   })
   return router;
 };
