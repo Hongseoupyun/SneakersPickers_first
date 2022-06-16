80b2f1af62985250386f671ecfa5bf2b5fd5ddc1
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-15 22:21:23 -0400

    commit

--- server/routes/mylistings.js
+++ server/routes/mylistings.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 
-const queryListing = `SELECT * FROM listings WHERE user_id = $1 AND active = true`
+const queryListing = `SELECT * FROM listings WHERE user_id = $1 AND active = true;`
 
 module.exports = (db) => {
   router.get("/mylistings", (req, res) => {
     db.query(queryListing,[req.user.id])
     .then((result)=>{
+      res.json({ success: true, message: "User uploaded listing" })
       res.json(result.rows)
     })
     .catch((err)=>[
