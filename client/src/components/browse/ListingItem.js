2654427598a353eb7e2772ea40c338562e315b87
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-18 21:29:09 -0400

    Add styling to brose

--- client/src/components/browse/ListingItem.js
+++ client/src/components/browse/ListingItem.js
@@ -1,27 +1,32 @@
 import React from "react";
 import "./ListingItem.scss";
-import { Link } from "react-router-dom";
+
 
 export default function ListingItem(props) {
 
+  const {name,size,brand,description,preference,id,picture} = props
+
 
   return (
-    <div class="cardsale">
-      <div class="photo">
-        <img src={props.picture} alt="" />
+
+    <section className="mylisting-container">
+      <img className="mylisting-img" src={picture} />
+      <div className="mylisting-card-contents">
+        <div className="listing-text" id="name">
+          <h1>{name}</h1>
+        </div>
+        <div className="listing-text" id="brand-size">
+          <div className="my-brand">{brand}</div>
+          <div className="my-size">Size {size}</div>
+        </div>
+        <div className="listing-text" id="description">
+          <span>{description}</span>
+        </div>
+        <div className="view-offers">
+        <a className="make-offer-button-href" href={`/placeoffer/${id}`}> <button type="submit">Make a Offer</button></a>
+        </div>
       </div>
-      <div class="description">
-        <h2>{props.name}</h2>
-        <h4>Size: {props.size}</h4>
-        <h1>{props.brand}</h1>
-        <h4>Description:</h4>
-        <p>{props.description}</p>
-        <h4>Preferences:</h4>
-        <p>{props.preference}</p>
-        <a href={`/placeoffer/${props.id}`}><button>Offer</button></a>
-        <button>Favorite</button>
-      </div>
-    </div>
+    </section>
   );
 }
 
