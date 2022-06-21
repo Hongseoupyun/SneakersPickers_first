0845af78cc074e83d7dbbe7d2e0e43f8f5a77535
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-20 22:21:20 -0400

    Add css

--- client/src/components/MyAccount/MyListingItem.js
+++ client/src/components/MyAccount/MyListingItem.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { BsFillBookmarkHeartFill } from "react-icons/bs";
+import { MdDescription } from "react-icons/md";
+
 
 
 export default function MyListingItem(props) {
-  const { name, description, brand, size, img } = props
+  const { name, description, brand, size, img, preference } = props
 
   return (
     <section className="mylisting-container">
@@ -17,15 +20,18 @@ export default function MyListingItem(props) {
           <div className="my-brand">{brand}</div>
           <div className="my-size">Size {size}</div>
         </div>
+        <div className="listing-text" id="pref">
+          <BsFillBookmarkHeartFill /><span>Preference: {preference}</span>
+        </div>
         <div className="listing-text" id="description">
-          <span>{description}</span>
+          <MdDescription /><span>{description}</span>
         </div>
         <div className="view-offers">
-        <a href="/offers"> <button type="submit">View Offers</button></a>
+          <a href="/offers"> <button type="submit">View Offers</button></a>
         </div>
         <div className="listing-text" id="offers">
-          <button type="button" class="btn btn-primary">
-          <span className="badge badge-light">4</span>OFFERS
+          <button type="button" className="btn btn-primary">
+            <span className="badge badge-light">4</span>OFFERS
           </button>
         </div>
       </div>
--- client/src/components/browse/ListingItem.js
+++ client/src/components/browse/ListingItem.js
@@ -5,6 +5,8 @@ import { MdDescription} from "react-icons/md";
 
 
 
+
+
 export default function ListingItem(props) {
 
   const {name,size,brand,description,preference,id,picture} = props
@@ -18,14 +20,14 @@ export default function ListingItem(props) {
         <div className="listing-text" id="browse-name">
           <h1>{name}</h1>
         </div>
-        <div className="listing-text" id="pref">
+        <div className="listing-text" id="browse-pref">
           <BsFillBookmarkHeartFill className=""/><span>Preference: {preference}</span>
         </div>
         <div className="listing-text" id="brand-size-br">
           <div className="my-brand">{brand}</div>
           <div className="my-size">Size {size}</div>
         </div>
-        <div className="listing-text" id="description">
+        <div className="listing-text" id="browse-desc">
           <MdDescription/><span>{description}</span>
         </div>
         <div className="make-offers">
