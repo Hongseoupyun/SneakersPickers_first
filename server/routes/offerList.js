560a4c246644c4faa755ecf1a69557aaf436427f
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-24 12:47:37 -0400

    Removed unnecessary codes

--- server/routes/offerList.js
+++ server/routes/offerList.js
@@ -110,73 +110,7 @@ module.exports = (db) => {
 };
 
 
-// const router = require("express").Router();
-
-// const queryListings = `
-// SELECT * FROM listings 
-// JOIN users ON users.id=user_id 
-// JOIN offers ON listings.id=listing_offer_made_to_id
-// WHERE users.id = $1
-// AND
-// pending IS TRUE;
-// `;
-
-// const offeredListings = `
-// SELECT * FROM listings 
-// JOIN offers ON listings.id=listing_being_offered_id
-// JOIN users ON user_id=users.id
-// WHERE listing_being_offered_id = $1
-// AND listing_offer_made_to_id = $2
-// AND users.id = $3;
-// `;
 
 
 
-// module.exports = (db) => {
 
-//   router.get("/offerlist", (req, res) => {
-//     let listings = []
-//     // console.log("User id:", req.user.id)
-    
-//     const buyer = (offerID, listID, userID) => db.query(offeredListings, [offerID, listID, userID])
-//     .then((result) => {
-//       console.log("result rows:", result.rows)
-//       return result.rows
-//     })
-//     .catch((err) => {
-//       res.status(500).json({ error: err.message });
-//     });
-
-//     const seller = () => db.query(queryListings, [req.user.id])
-//       .then((result) => {
-//         console.log("result rows:", result.rows[0].listing_being_offered_id)
-//         buyer(result.rows[0].listing_being_offered_id, result.rows[0].listing_offer_made_to_id, req.user.id)
-//         .then((resbuyer) =>{
-//           listings = [...resbuyer, ...result.rows]
-//           console.log("listings:", listings)
-//           res.json(listings);
-//         })
-//       })
-//       .catch((err) => {
-//         res.status(500).json({ error: err.message });
-//       });
-
-//       seller()
-//   });
-
-//   // router.post("/offerlist", (req, res) => {
-//   //   // console.log("User id:", req.user.id)
-//   //   console.log("offerlist for offered item")
-//   //   db.query(offeredListings, [req.body.offeredid])
-//   //     .then((result) => {
-//   //       console.log("result rows:", result.rows)
-//   //       res.json(result.rows);
-//   //     })
-//   //     .catch((err) => {
-//   //       res.status(500).json({ error: err.message });
-//   //     });
-//   // });
-
-
-//   return router;
-// };
